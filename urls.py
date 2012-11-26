913e318437c905efcc410a7107e434079c7e3f32
Iman Yusuf <iman.yusuf@rmit.edu.au>
2012-11-25 20:07:12 -0800

    configured as Mytardis tab

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 # Create your views here.
 from django.conf.urls import patterns, url
 
-from mytardis_hpc_app import views
+from tardis.apps.mytardis_hpc_app import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.submission, name='submission'),
+urlpatterns = patterns('tardis.apps.mytardis_hpc_app',
+    url(r'^(?P<experiment_id>\d+)/$', views.index, name='index'),
+    url(r'^experiment/(?P<experiment_id>\d+)/mytardis_hpc_app/$', views.submission, name='submission'),
 )
--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 # Create your views here.
 
 from django.shortcuts import render
-from mytardis_hpc_app.form import ContactForm
+from tardis.apps.mytardis_hpc_app.form import ContactForm
 
 
-def index(request):
+def index(request, experiment_id):
     if request.method == 'POST': # If the form has been submitted...
         form = ContactForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation ules pass
@@ -16,8 +16,12 @@ def index(request):
 
     return render(request, 'mytardis_hpc_app/index.html', {
         'form': form,
+	'experiment_id': experiment_id,
         })
 
 
-def submission(request):
-    return render(request, 'mytardis_hpc_app/submission.html')
+def submission(request, experiment_id):
+    return render(request, 'mytardis_hpc_app/submission.html', {
+        'form': form,
+        'experiment_id': experiment_id,
+        })
