a6fd1bbc2031af506145e4c3fc3382efc7ced5ce
Iman Yusuf <iman.yusuf@rmit.edu.au>
2012-12-09 17:56:27 -0800

    Status table is added

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ComputationStatus(models.Model):
+    group_id = models.CharField(max_length=100, verbose_name="Group ID")
+    create_stage = models.CharField(max_length=100, verbose_name="Create")
+    setup_stage = models.CharField(max_length=100, verbose_name="Setup")
+    run_stage = models.CharField(max_length=100, verbose_name="Run")
+    terminate_stage = models.CharField(max_length=100, verbose_name="Terminate")
+
+    def __unicode__(self):
+        return self.group_id
+
--- views.py
+++ views.py
@@ -1,23 +1,24 @@
 # Create your views here.
 
+import json
+import urllib
+import urllib2
+import settings
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from tardis.tardis_portal.shortcuts import HttpResponse
 from django.template import Context, RequestContext, loader
 
 from tardis.tardis_portal.models.dataset import Dataset
 from tardis.apps.mytardis_hpc_app.form import HPCForm
 from tardis.apps.mytardis_hpc_app.response_form import ResponseForm
+from tardis.tardis_portal.shortcuts import HttpResponse
+
+from tardis.apps.mytardis_hpc_app.models import ComputationStatus
 
 
-import json
-import urllib
-import urllib2
 
-import settings
-
-
-receiver = "iimanyusuf@gmail.com"
+receiver = ""
 
 
 def index(request, experiment_id):
@@ -30,32 +31,34 @@ def index(request, experiment_id):
             post_request_values = form.cleaned_data
             input_dir_path = get_input_dir_path(experiment_id)
             if input_dir_path:
-                encoded_input_dir = encodeZip(input_dir_path)
+                encoded_input_dir = encode_zip(input_dir_path)
                 post_request_values['input_dir'] = encoded_input_dir
 
             from threading import Thread
-            t = Thread(target=submitjob, args=(post_request_values,))
+            t = Thread(target=submit_job, args=(post_request_values,))
             t.start()
 
             template = loader.get_template(
                     'mytardis_hpc_app/submission.html')
             context = RequestContext(request,
                     {'experiment_id': experiment_id,
+                     'computation_status': ComputationStatus.objects.all(),
                      'email':receiver}
                 )
             return HttpResponse(template.render(context))
         else:
             print 'invalid post request'
-    print 'get requeset'
+    print 'get request'
 
     form = HPCForm()
     return render(request, 'mytardis_hpc_app/index.html', {
         'form': form,
         'experiment_id': experiment_id,
+        'computation_status': ComputationStatus.objects.all(),
         })
 
 
-def encodeZip(zip_file):
+def encode_zip(zip_file):
     import base64
     f = open(zip_file, "rb")
     b64_text = base64.b64encode(f.read())
@@ -63,7 +66,7 @@ def encodeZip(zip_file):
     return b64_text
 
 
-def submitjob(values):
+def submit_job(values):
     url = settings.BDP_HPC_URL
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data)
@@ -89,13 +92,16 @@ def response(request):
     if request.method == 'POST':
         input_parameter = request.POST
         message = input_parameter['message']
-        sendEmail(message, receiver)
+        stage = input_parameter['stage']
+        group_id = input_parameter['group_id']
+        #send_email(message, receiver)
+        set_computation_status(group_id, stage, 'completed')
     return render(request, 'mytardis_hpc_app/response.html', {
         'form':form
             })
 
 
-def sendEmail(message, receiver) :
+def send_email(message, receiver) :
     import smtplib
     import string
     subject = "Group ID for BDP computation"
@@ -118,6 +124,29 @@ def submission(request, experiment_id):
         })
 
 
+def set_computation_status(current_id, stage, status):
+    record_exists = ComputationStatus.objects.filter(group_id=current_id)
+    if not record_exists:
+        computation = ComputationStatus(group_id=current_id)
+        computation.save()
+
+    computation_list = list(ComputationStatus.objects.filter(
+        group_id=current_id))
+
+    if stage.lower() == "create":
+        computation_list[0].create_stage = status
+
+    elif stage.lower() == "setup":
+        computation_list[0].setup_stage = status
+
+    elif stage.lower() == "run":
+        computation_list[0].run_stage = status
+
+    elif stage.lower() == "terminate":
+        computation_list[0].terminate_stage = status
+
+    computation_list[0].save()
+
 def results_ready(request, group_id):
     res = '""'
     if request.method == 'POST':
@@ -127,3 +156,4 @@ def results_ready(request, group_id):
             res = '""'
     return HttpResponse(json.dumps(res),
         mimetype='application/json')
+
